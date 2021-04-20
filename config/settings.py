d9e2e756d163f5c65a62b0909b8c6e38be7aa05c
mk2020 <mahnaaz85@gmail.com>
2021-04-20 18:29:53 +0200

    add postgress in settings

--- config/settings.py
+++ config/settings.py
@@ -88,11 +88,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'radorado',
+            'PASSWORD': 'radorado',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 
+}
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
