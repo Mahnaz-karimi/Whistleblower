2236dd0be7b244efb686b5123a0e8e443a671a24
mk2020 <mahnaaz85@gmail.com>
2021-05-07 00:45:24 +0200

    change if-statment in setting

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import os
 import django_heroku
 
 
-if os.path.exists('/etc/config.json'):
+if (os.path.exists('/etc/config.json')):
     with open('/etc/config.json') as config_file:
         config = json.load(config_file)
         SECRET_KEY = config.get('SECRET_KEY')
