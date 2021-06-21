f1328b9d116c09ee39bf4a8caefe973924aff8e1
mk2020 <mahnaaz85@gmail.com>
2021-06-21 05:14:29 +0200

    test fixture

--- tests/test_case_url.py
+++ tests/test_case_url.py
@@ -35,3 +35,17 @@ def test_Login_CaseInfo_Cases_ListView(client, user_data_for_login, create_user_
     resp = client.get(user_url)
     assert resp.status_code == 200   # Fordi vi er logget ind bliver vi ikke redirectet
     assert "Go back" in str(resp.content)
+
+
+@pytest.mark.django_db
+def test_Login_CaseInfo_Delete_View(client, user_data_for_login, create_user_for_login, case_info_data):
+    user_model = get_user_model()
+    assert user_model.objects.count() == 1  # Når vi kalder create_user_model in i modelen opreetter vi en user
+    login_url = urls.reverse('caseworker:login')
+    resp = client.post(login_url, data=user_data_for_login)  # Her poster en login-data til login-side
+    assert resp.status_code == 302
+    assert resp.url == urls.reverse('case:caseinfo-view')
+    case_info = CaseInfo.objects.latest('pk')
+    user_url = urls.reverse('case:caseinfo-delete', kwargs={'pk': case_info.id})  # Se sagerne under en sagsinfo
+    resp = client.get(user_url)
+    assert resp.status_code == 200   # Fordi vi er logget ind
